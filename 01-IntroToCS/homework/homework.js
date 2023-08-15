4fc27c2e39717283e0b0d1ae3c86112320f4358f
777usuario <rodriguez.t.cristian.fernando@gmail.com>
2023-08-15 12:57:44 -0500

    introducion

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,25 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   var resultado = 0;
+   var indice = num.length - 1;
+   for (let i = 0; i < num.length; i++) {
+      resultado = resultado + (num[i] * 2 ** indice);
+      indice--;
+   }
+   return parseInt(num,2);
+}
+console.log(BinarioADecimal('1100'));
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   let resultado = "";
+   while (num > 0) {
+      resultado = (num % 2) + resultado;
+      num = Math.floor(num / 2);
+   }
+   return resultado;
+}
+console.log(DecimalABinario(8));
 
 module.exports = {
    BinarioADecimal,
